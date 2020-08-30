5786f0de4ac7166756711f47ca828f0f7d6cd7fe
Nitzan Lev <nitzanl@breezometer.com>
2020-08-30 15:08:17 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,253 @@
+<template>
+  <div class="flex flex-col h-screen bg-black p-3">
+    <div class="flex justify-between items-center card mb-2" style="flex-basis: 60px">
+      <div class="flex items-center">
+        <!-- prettier-ignore -->
+        <svg class="fill-current text-white w-8 mr-5" viewBox="0 0 497 497" ><g><path d="m262.32 0h-27.64c-8.936 0-16.18 7.244-16.18 16.18 0 2.512.585 4.99 1.708 7.236l13.292 26.584h30l13.292-26.583c1.123-2.247 1.708-4.724 1.708-7.236 0-8.937-7.244-16.181-16.18-16.181z" fill="#fe7665"/><path d="m233.5 50v79.245h30v-79.245c0-8.284-6.716-15-15-15s-15 6.716-15 15z" fill="#ff4547"/><path d="m136.218 26.383-23.936 13.82c-7.739 4.468-10.39 14.364-5.922 22.103 1.256 2.176 3.001 4.029 5.097 5.413l24.803 16.376 25.981-15-1.781-29.668c-.151-2.507-.883-4.945-2.139-7.121-4.468-7.74-14.364-10.391-22.103-5.923z" fill="#fe7665"/><path d="m136.26 84.094 39.622 68.628 25.981-15-39.623-68.628c-4.142-7.174-13.316-9.633-20.49-5.49-7.174 4.142-9.633 13.316-5.49 20.49z" fill="#ff4547"/><path d="m40.203 112.282-13.82 23.936c-4.468 7.739-1.817 17.635 5.922 22.103 2.176 1.256 4.613 1.988 7.121 2.139l29.668 1.781 15-25.981-16.376-24.803c-1.384-2.096-3.237-3.841-5.413-5.097-7.739-4.469-17.634-1.817-22.102 5.922z" fill="#fe7665"/><path d="m69.094 162.24 68.628 39.622 15-25.981-68.628-39.621c-7.174-4.142-16.348-1.684-20.49 5.49-4.143 7.174-1.684 16.348 5.49 20.49z" fill="#ff4547"/><path d="m0 234.68v27.639c0 8.936 7.244 16.18 16.18 16.18 2.512 0 4.99-.585 7.236-1.708l26.584-13.291v-30l-26.583-13.292c-2.247-1.123-4.724-1.708-7.236-1.708-8.937 0-16.181 7.244-16.181 16.18z" fill="#fe7665"/><path d="m50 263.5h79.245v-30h-79.245c-8.284 0-15 6.716-15 15s6.716 15 15 15z" fill="#ff4547"/><path d="m26.383 360.782 13.82 23.936c4.468 7.739 14.364 10.39 22.103 5.922 2.176-1.256 4.029-3.001 5.413-5.098l16.376-24.803-15-25.981-29.668 1.781c-2.507.151-4.945.883-7.121 2.139-7.74 4.469-10.391 14.365-5.923 22.104z" fill="#fe7665"/><path d="m84.094 360.74 68.628-39.622-15-25.981-68.628 39.623c-7.174 4.142-9.633 13.316-5.49 20.49 4.142 7.174 13.316 9.633 20.49 5.49z" fill="#ff4547"/><path d="m112.282 456.797 23.936 13.82c7.739 4.468 17.635 1.817 22.103-5.922 1.256-2.176 1.988-4.613 2.139-7.121l1.781-29.668-25.981-15-24.803 16.376c-2.096 1.384-3.841 3.237-5.097 5.413-4.469 7.739-1.817 17.634 5.922 22.102z" fill="#fe7665"/><path d="m162.24 427.906 39.622-68.628-25.981-15-39.622 68.628c-4.142 7.174-1.684 16.348 5.49 20.49 7.175 4.143 16.349 1.684 20.491-5.49z" fill="#ff4547"/><path d="m234.68 497h27.639c8.936 0 16.18-7.244 16.18-16.18 0-2.512-.585-4.99-1.708-7.236l-13.291-26.584h-30l-13.292 26.583c-1.123 2.247-1.708 4.724-1.708 7.236 0 8.937 7.244 16.181 16.18 16.181z" fill="#fe7665"/><path d="m263.5 447v-79.245h-30v79.245c0 8.284 6.716 15 15 15s15-6.716 15-15z" fill="#ff4547"/><path d="m360.782 470.617 23.936-13.82c7.739-4.468 10.39-14.364 5.922-22.103-1.256-2.176-3.001-4.029-5.098-5.413l-24.803-16.376-25.981 15 1.781 29.668c.151 2.507.883 4.945 2.139 7.121 4.469 7.74 14.365 10.391 22.104 5.923z" fill="#fe7665"/><path d="m360.74 412.906-39.622-68.628-25.981 15 39.622 68.628c4.142 7.174 13.316 9.633 20.49 5.49 7.175-4.142 9.634-13.316 5.491-20.49z" fill="#ff4547"/><path d="m456.797 384.718 13.82-23.936c4.468-7.739 1.817-17.635-5.922-22.103-2.176-1.256-4.613-1.988-7.121-2.139l-29.668-1.781-15 25.981 16.376 24.803c1.384 2.096 3.237 3.841 5.413 5.097 7.739 4.469 17.634 1.817 22.102-5.922z" fill="#fe7665"/><path d="m427.906 334.76-68.628-39.622-15 25.981 68.628 39.622c7.174 4.142 16.348 1.684 20.49-5.49 4.143-7.175 1.684-16.349-5.49-20.491z" fill="#ff4547"/><path d="m497 262.32v-27.64c0-8.936-7.244-16.18-16.18-16.18-2.512 0-4.99.585-7.236 1.708l-26.584 13.292v30l26.583 13.292c2.247 1.123 4.724 1.708 7.236 1.708 8.937 0 16.181-7.244 16.181-16.18z" fill="#fe7665"/><path d="m447 233.5h-79.245v30h79.245c8.284 0 15-6.716 15-15s-6.716-15-15-15z" fill="#ff4547"/><path d="m470.617 136.218-13.82-23.936c-4.468-7.739-14.364-10.39-22.103-5.922-2.176 1.256-4.029 3.001-5.413 5.097l-16.376 24.803 15 25.981 29.668-1.781c2.507-.151 4.945-.883 7.121-2.139 7.74-4.468 10.391-14.364 5.923-22.103z" fill="#fe7665"/><path d="m412.906 136.26-68.628 39.622 15 25.981 68.628-39.622c7.174-4.142 9.633-13.316 5.49-20.49-4.142-7.175-13.316-9.634-20.49-5.491z" fill="#ff4547"/><path d="m384.718 40.203-23.936-13.82c-7.739-4.468-17.635-1.817-22.103 5.922-1.256 2.176-1.988 4.613-2.139 7.121l-1.781 29.668 25.981 15 24.803-16.376c2.096-1.384 3.841-3.237 5.097-5.413 4.469-7.739 1.817-17.634-5.922-22.102z" fill="#fe7665"/><path d="m334.76 69.094-39.622 68.628 25.981 15 39.622-68.628c4.142-7.174 1.684-16.348-5.49-20.49-7.175-4.143-16.349-1.684-20.491 5.49z" fill="#ff4547"/><circle cx="248.5" cy="248.5" fill="#ff4547" r="150"/><path d="m248.5 121v255c70.416 0 127.5-57.083 127.5-127.5s-57.084-127.5-127.5-127.5z" fill="#ffa785"/><path d="m346 248.5c0-70.417-43.652-127.5-97.5-127.5-70.416 0-127.5 57.083-127.5 127.5s57.084 127.5 127.5 127.5c53.848 0 97.5-57.083 97.5-127.5z" fill="#ffbea4"/><g fill="#fe7665"><circle cx="248.5" cy="248.5" r="22.5"/><circle cx="286" cy="313.452" r="22.5"/><circle cx="323.5" cy="248.5" r="22.5"/><circle cx="286" cy="183.548" r="22.5"/><circle cx="211" cy="183.548" r="22.5"/><circle cx="173.5" cy="248.5" r="22.5"/><circle cx="211" cy="313.452" r="22.5"/></g></g></svg>
+        <p class="text-white text-3xl leading-9">Coronavirus COVID-19 Global Cases by Johns Hopkins CSSE</p>
+      </div>
+      <div>
+        <!-- prettier-ignore -->
+        <svg class="fill-current text-gray-200 w-5" viewBox="0 -53 384 384"><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></svg>
+      </div>
+    </div>
+    <div class="flex-1 flex overflow-y-auto">
+      <div class="flex flex-col" style="flex-basis: 310px">
+        <div class="card flex flex-col items-center justify-center" style="flex-basis: 160px">
+          <p class="text-white text-3xl leading-9">Total Confirmed</p>
+          <p class="text-red-100 font-semibold text-7xl" style="line-height: 6.125rem">76,787</p>
+        </div>
+        <div class="card flex-1 my-2 flex flex-col overflow-y-auto">
+          <p class="text-lg font-semibold text-gray-300 leading-6 text-center mb-4">Confirmed Cases by Country/Region</p>
+          <ul class="overflow-y-auto custom-scrollbar">
+            <li v-for="country in countries" :key="country.name" class="capitalize flex items-center border-b border-gray-400 py-2">
+              <p class="text-xl font-semibold leading-6 mr-3 text-red-100">{{ country.cases }}</p>
+              <p class="text-xl leading-6 text-gray-300">{{ country.name }}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="card" style="flex-basis: 80px"></div>
+      </div>
+      <div class="card flex-1 mx-2"></div>
+      <div class="flex flex-col" style="flex-basis: 550px">
+        <div class="flex-1 flex mb-2">
+          <div class="card flex-1 mr-2">1</div>
+          <div class="card flex-1">2</div>
+        </div>
+        <div class="card" style="flex-basis: 380px">3</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countries: [
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        },
+        {
+          name: "japan",
+          cases: 120_123
+        },
+        {
+          name: "israel",
+          cases: 120_123
+        },
+        {
+          name: "france",
+          cases: 120_123
+        },
+        {
+          name: "united states",
+          cases: 120_123
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-scrollbar {
+  &::-webkit-scrollbar {
+    width: 7px;
+  }
+
+  &::-webkit-scrollbar-track {
+    @apply bg-gray-500;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    width: 7px;
+    @apply bg-gray-400;
+  }
+}
+
+.border-a-gray-100 {
+  border-color: rgba(214, 214, 214, 0.2);
+}
+
+.card {
+  @apply bg-gray-100 border border-a-gray-100 px-5 py-2;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    colors: {
+      black: "#000",
+      white: "#fff",
+      "gray-100": "#222222",
+      "gray-200": "#C4C4C4",
+      "gray-300": "#D6D6D6",
+      "gray-400": "#5C5C5C",
+      "gray-500": "#464646",
+      "red-100": "#E60000"
+    },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem"
+    }
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === "production",
+    content: ["components/**/*.vue", "layouts/**/*.vue", "pages/**/*.vue", "plugins/**/*.js", "nuxt.config.js"]
+  }
+};
